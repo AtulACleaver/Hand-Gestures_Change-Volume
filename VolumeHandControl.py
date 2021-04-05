229cb02e747903943163d21af1e49089e6ffbbd7
Atul Anand <atulcleaver@gmail.com>
2021-04-05 17:19:05 +0530

    successful hand detection

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import numpy as np
+import HandTrackingModule as htm
 
 # Setting the camera size
 wCam, hCam = 640, 480
@@ -11,14 +12,17 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
+detector = htm.handDetector()
+
 
 while True:
     success, img = cap.read()
+    
+    img = detector.findHands(img)
+
 
     cTime = time.time()
-
     fps = 1/(cTime-pTime)
-
     pTime = cTime
 
     cv2.putText(img, f'FPS: {int(fps)}',(40,50), cv2.FONT_HERSHEY_COMPLEX, 1, (231, 76, 60), 3)
