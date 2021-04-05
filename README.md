c54842e08f2e838edc21fdd284f7d8344b0ec81a
Atul Anand <atulcleaver@gmail.com>
2021-04-05 16:48:53 +0530

    Added a README, and just made an input of webcam.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Hand Gestures To Change Volume.
+You can change the volume of the computer with your hand movements.
+
+## Things You Need to Setup
+- `Python 3`
+- `Open CV` by using `pip install opencv-python`
+- `Numpy` by using `pip install numpy`
+
---
+++ VolumeHandControl.py
@@ -0,0 +1,27 @@
+import cv2
+import time
+import numpy as np
+
+# Setting the camera size
+wCam, hCam = 640, 480
+
+# Setting a variable to capture video from webcam.
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+
+while True:
+    success, img = cap.read()
+
+    cTime = time.time()
+
+    fps = 1/(cTime-pTime)
+
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}',(40,50), cv2.FONT_HERSHEY_COMPLEX, 1, (231, 76, 60), 3)
+    cv2.imshow("Img", img)
+    cv2.waitKey(1)
+
