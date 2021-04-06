1842d9590ea41bc3cf2ea57fc780cd4b49a6020f
Atul Anand <atulcleaver@gmail.com>
2021-04-06 10:11:55 +0530

    added the hand gestures

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -2,9 +2,13 @@ import cv2
 import time
 import numpy as np
 import HandTrackingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 # Setting the camera size
-wCam, hCam = 640, 480
+wCam, hCam = 640,480
 
 # Setting a variable to capture video from webcam.
 cap = cv2.VideoCapture(0)
@@ -12,14 +16,52 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
-detector = htm.handDetector()
+detector = htm.handDetector(detectionCon=0.7)
 
+# Initializing
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
 
 while True:
     success, img = cap.read()
     
     img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw = False)
+    if len(lmList) !=0:
+        # print(lmList[4], lmList[8])
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1+x2)//2, (y1+y2)//2
+        cv2.circle(img, (x1,y1), 15, (255,0,255), cv2.FILLED)
 
+        cv2.circle(img, (x2,y2), 15, (255,0,255), cv2.FILLED)
+
+        cv2.line(img, (x1,y1), (x2,y2), (255,0,255), 3)
+
+        cv2.circle(img, (cx, cy), 15, (255,0,255), cv2.FILLED)
+
+        length = math.hypot(x2-x1, y2-y1)
+
+
+        # Hand Range 50-300
+        # Volume Range -65 - 0
+        vol = np.interp(length, [50, 300], [minVol, maxVol])
+
+        print(int(length),vol)
+
+        volume.SetMasterVolumeLevel(vol, None)
+
+
+        if length<50:
+             cv2.circle(img, (cx, cy), 15, (0,255,0), cv2.FILLED)
+        
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
